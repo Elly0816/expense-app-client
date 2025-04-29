182d41dd4e4ef7dfe4d99d77c6091c49b3855199
Eleazar Udo <elzoremmanuel@gmail.com>
2025-04-29 02:01:32 +0100

    design login screen and loading state

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,44 @@
+'use client';
+import { COLORS } from '@/Colors';
 import { Login } from '@/components/Login';
+import { AuthContextType, useAuth } from '@/contexts/authContext';
+import { useTheme } from '@/contexts/themeContext';
 import { Flex } from 'antd';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 const LoginPage: React.FC = () => {
+  const { theme } = useTheme();
+  const { isAuthenticated } = useAuth() as AuthContextType;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.push('/');
+    } else {
+      setIsLoading(false);
+    }
+  }, [isAuthenticated, router]);
+
+  const element = isLoading ? (
+    <h2>Loading...</h2>
+  ) : (
+    <Login containerStyle={{ justifyContent: 'center', alignItems: 'center' }} />
+  );
+
   return (
-    <Flex className="justify-center items-center">
-      <Login containerStyle={{ justifyContent: 'center', alignItems: 'center' }} />
+    <Flex
+      style={{
+        backgroundColor: COLORS[theme].background,
+        backgroundImage: "url('./expense.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'none',
+      }}
+      className="justify-center items-center h-full"
+    >
+      {element}
     </Flex>
   );
 };
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,11 @@
 'use client';
 import { authApi } from '@/api/auth/auth';
+import { COLORS } from '@/Colors';
+import { useTheme } from '@/contexts/themeContext';
 import { Button, Card, Flex } from 'antd';
+import Title from 'antd/es/typography/Title';
 import { CSSProperties } from 'react';
+import { FaGoogle } from 'react-icons/fa';
 
 type LoginPropsType = {
   containerStyle?: CSSProperties;
@@ -10,16 +14,49 @@ type LoginPropsType = {
 };
 
 export const Login: React.FC<LoginPropsType> = ({ containerStyle, cardStyle, buttonStyle }) => {
+  const { theme } = useTheme();
   return (
-    <Flex className="justify-center" style={{ ...containerStyle }}>
+    <Flex
+      className="justify-center"
+      style={{
+        // minHeight: 'min-content',
+        height: '70%',
+        ...containerStyle,
+        // backgroundColor: COLORS[theme].cardBackground,
+        // color: COLORS[theme].textBody,
+      }}
+    >
       <Card
-        hoverable
-        style={{ width: 240, ...cardStyle }}
+        // hoverable
+        // className="flex flex-col justify-between items-center"
+        styles={{
+          body: {
+            backgroundColor: COLORS[theme].cardBackground,
+            color: COLORS[theme].textBody,
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          },
+        }}
+        style={{ width: 240, height: '100%', ...cardStyle }}
         // cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
       >
+        <Title style={{ color: COLORS[theme].textBody }} level={3}>
+          Welcome back to your Expense Tracker
+        </Title>
         {/* <Meta title="Europe Street beat" description="www.instagram.com" /> */}
-        <Button style={{ ...buttonStyle }} onClick={authApi.loginWithGoogle}>
+        <Button
+          style={{
+            color: COLORS[theme].textBody,
+            ...buttonStyle,
+            backgroundColor: COLORS[theme].background,
+          }}
+          onClick={authApi.loginWithGoogle}
+        >
           Login with Google
+          <FaGoogle />
         </Button>
       </Card>
     </Flex>
