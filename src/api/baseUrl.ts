98d3591c2fb0bf5f9b6b603dd4af03211e5f263c
Eleazar Udo <elzoremmanuel@gmail.com>
2025-05-30 16:38:33 +0100

    added .env file and api in it

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  // baseURL: 'https://expense-app-server-1.onrender.com',
   timeout: 1000,
   withCredentials: true,
 });
